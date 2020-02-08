20c5d30fe25060195b4cf9946e3fa3d2872076c3
Andrew Naumov <anaumow@mail.ru>
2020-02-09 01:19:45 +0300

    add spellchecker

--- vlad/main.py
+++ vlad/main.py
@@ -8,4 +8,7 @@ from naumow.dao import Dao
 while True:
     message = input()
     dao = Dao()
-    print(dao.get_answer(bot.get_response(message)))
+    num = bot.get_response(message)
+    #print(num)
+    print(dao.execute("select `text` from hack.`key` where `answer_id` = " + str(num)))
+    print(dao.get_answer(num)[0])
--- vlad/tokenizer.py
+++ vlad/tokenizer.py
@@ -7,8 +7,10 @@ def tokenize_ru(text):
     tokens = word_tokenize(text)
     tokens = [i for i in tokens if (i not in string.punctuation)]
     stop_words = stopwords.words('russian')
-    stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'в', '—', '–', 'к', 'на', '...'])
-    tokens = [i for i in tokens if (i not in stop_words)]
+    stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'в', '—', '–', 'к', 'на', '...'])
+    stop_words.remove('как')
+
+    tokens = [i for i in tokens if (i.lower() not in stop_words)]
     tokens = [i.replace("«", "").replace("»", "").replace("\"", "").replace("\'", "").replace("`", "") for i in tokens]
     tokens = filter(lambda x: x is not '', tokens)
     return tokens
